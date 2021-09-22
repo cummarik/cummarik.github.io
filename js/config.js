ade171418c5c429e3bfcd205a60a226c51301263
Darian Globus <legionerddno@yandex.com>
2021-09-22 23:21:40 +0300

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 2.5, 
+
+  // Заголовок
+  title: 'cummarik',
+
+  // Социальные сети
+  social: {
+    telegram: '@cummarik',
+    steam: 'cummarik',
+    vk: 'danko_jun', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#f5323c',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#20603d',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(10, 69, 0, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#1faee9',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+  // Текст копирайта
+  copyrightText: '',
+  // Цвет копирайта
+  copyrightTextColor: '#f5323c',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#1faee9',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
