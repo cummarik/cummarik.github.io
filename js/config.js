15184972af05ba81e8d2db7a3af2842df70be0cd
Danila <legionerddno@yandex.com>
2021-12-04 14:49:20 +0300

    upd

--- js/config.js
+++ js/config.js
@@ -12,33 +12,24 @@ let config = {
   // Социальные сети
   social: {
     telegram: '@cummarik',
-	instagram: 'nndushnila',
-	vk: 'danko_jun', 
+	  instagram: 'nndushnila',
+	  vk: 'danko_jun', 
     steam: 'cummarik',
     github: 'cummarik',
   },
 
-
   // Цвет иконки загрузки
   preloaderColor: '#f5323c',
-
 
   // Цвет кнопки play
   playButtonColor: '#20603d',
   // Цвет тени кнопки play
   playButtonShadowColor: 'rgba(10, 69, 0, 5.0)',
 
-
   // Цвет заголовка
   titleColor: '#1faee9',
   // Цвет тени загаловка
   titleShadowColor: 'rgba(255, 0, 55, 0.6)',
-
-
-  // Текст копирайта
-  copyrightText: '',
-  // Цвет копирайта
-  copyrightTextColor: '#f5323c',
 
   // Цвет иконок социальных сетей
   socialColor: '#1faee9',
