d6b67093e6ca8dc61298dc32ad855452bb94eaff
Danila <legionerddno@yandex.com>
2022-06-13 01:46:35 +0300

    failed perestroika

---
+++ old/js/config.js
@@ -0,0 +1,38 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 5.0, 
+
+  // Заголовок
+  title: 'cummarik',
+
+  // Социальные сети
+  social: {
+    telegram: '@nedushnilla',
+    instagram: 'nedushnilla',
+    vk: 'danko_jun', 
+    steam: 'cummarik',
+    github: 'cummarik',
+  },
+
+  // Цвет иконки загрузки
+  preloaderColor: '#f5323c',
+
+  // Цвет кнопки play
+  playButtonColor: '#20603d',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(10, 69, 0, 5.0)',
+
+  // Цвет заголовка
+  titleColor: '#1faee9',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#1faee9',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
